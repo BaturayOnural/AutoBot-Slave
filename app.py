34e6f3bb301c4684d0ad557013b585b769613b2d
BaturayOnural <baturayonural@gmail.com>
2021-09-15 15:24:29 +0300

    Added email route with status

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from random import randrange
 import requests
 import os
 from selenium import webdriver
+import time
 
 
 # Api keys
@@ -13,9 +14,12 @@ webshare_api_key = "3c43d9fc51d65c8cf7fe3bb85d1ecfcade8b41be"
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# globals
 IP = ""
 PORT = ""
 PROXY = IP + ":" + PORT
+status = "0"
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--proxy-server=%s' % PROXY)
 #chrome = webdriver.Chrome("./chromedriver", chrome_options=chrome_options)
@@ -28,6 +32,23 @@ chrome_options.add_argument('--proxy-server=%s' % PROXY)
 def hello():
     return "Hello World!"
 
+@app.route('/get_status')
+def hello():
+    return status
+
+@app.route('/email')
+def hello():
+    global IP, PORT, PROXY, status
+    IP = ""
+    PORT = ""
+    PROXY = IP + ":" + PORT
+    status = "1"
+    time.sleep(7)
+    status="2"
+    time.sleep(7)
+    status="3"
+    return "Email Generated!"
+
 # Run server from terminal
 if __name__ ==  "__main__":
-    app.run(host="0.0.0.0 ")
+    app.run(host="0.0.0.0")
