9fe4cdf3191d2f8d0a5a870ce0658e37f8942a0c
BaturayOnural <baturayonural@gmail.com>
2021-11-09 17:30:57 +0300

    added chrome installation and fixed requirements

---
+++ install_chrome.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+apt update
+apt upgrade
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+apt-get install -f
+google-chrome
