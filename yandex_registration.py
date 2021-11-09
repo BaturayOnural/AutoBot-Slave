d3b435a880ef3265e49569c4e60a4fa3da23e5e5
BaturayOnural <baturayonural@gmail.com>
2021-11-09 20:04:54 +0300

    minor

--- yandex_registration.py
+++ yandex_registration.py
@@ -39,6 +39,9 @@ random_number = random.randint(100,999)
 # name
 chrome.get('https://passport.yandex.com.tr/registration/mail?from=mail&require_hint=1&origin=hostroot_homer_reg_tr&retpath=https%3A%2F%2Fmail.yandex.com.tr%2F&backpath=https%3A%2F%2Fmail.yandex.com.tr%3Fnoretpath%3D1')
 
+chrome.get_screenshot_as_file("screenshot.png")
+quit()
+
 # click "i don't have a phone number"
 next_bttn = chrome.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/main/div/div/div/form/div[3]/div/div[2]/div/div[1]/span")
 next_bttn.click()
@@ -100,7 +103,7 @@ if captcha_text != 0:
 else:
     print("task finished with error "+solver.error_code)
 
-#chrome.get_screenshot_as_file("screenshot.png")
+
 
 input = chrome.find_element_by_xpath("/html/body/div/div/div[2]/div/main/div/div/div/form/div[3]/div/div[2]/div[1]/span/input")
 input.clear()
